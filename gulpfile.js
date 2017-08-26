9c1436e3c29f4b4a3f34fb134f0cc7467af6b12e
markonijum <markonijum@gmail.com>
2017-08-26 12:24:44 +0200

    minor settings to project to fit perfectly starting workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,3 @@ require('./gulpfile/tasks/createSprite');
 require('./gulpfile/tasks/modernizr');
 require('./gulpfile/tasks/watchScss');
 
-// console.log(spritename);
-
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 
-var ResponsiveNav = require('./modules/ResponsiveNav');
-var SmoothScroll = require('./modules/SmoothScroll');
-var wow = require('wowjs');
-var CustomLoadMore = require('./modules/loadmore/CustomLoadMore');
+// var ResponsiveNav = require('./modules/ResponsiveNav');
+// var SmoothScroll = require('./modules/SmoothScroll');
+// var wow = require('wowjs');
+// var CustomLoadMore = require('./modules/loadmore/CustomLoadMore');
 
-new wow.WOW().init();
-var responsiveNav = new ResponsiveNav('.nav-collapse');
-var scroll = new SmoothScroll('.nav-main a');
-var loadmore = new CustomLoadMore('#joblist');
+//ENABLE WOW ANIMATION
+// new wow.WOW().init();
+
+// ENABLE RESPONSIVE NAV
+// var responsiveNav = new ResponsiveNav('.nav-collapse');
+
+//ENABLE SMOOTH SCROLLING ON NAV ELEMENTS
+// var scroll = new SmoothScroll('.nav-main a');
+
+//ENABLE LOADMORE ON SELECTED ELEMENT
+// var loadmore = new CustomLoadMore('#joblist');
